dabba0de1030359c52ebc819cc59e6fecb38ac0d
mabubakar365 <m.abubakar365@yahoo.com>
2024-07-28 20:32:48 +0500

    Updated repo for Assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,14 +6,20 @@
 ##############################################################
 # $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'ddf6bb2298a8463628cd53636d576c117b50b8ad'
+AESD_ASSIGNMENTS_VERSION = 'd826f97e55a845502586824180f7be7ec0e1a35c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
+
+
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-mabubakar365.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_LICENSE = GPL-2.0
+AESD_ASSIGNMENTS_LICENSE_FILES = COPYING
+
+AESD_ASSIGNMENTS_MODULE_SUBDIRS += aesd-char-driver/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -30,7 +36,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/bin
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
